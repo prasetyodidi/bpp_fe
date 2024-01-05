95b1e60e77cea256f69506158802ccc846ee207e
Didi Prasetyo <didiprasetyo36@gmail.com>
2024-01-05 14:46:11 +0700

    modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef, useState } from "react";
+
+export default function Modal({
+  children,
+  trigger,
+  onClose,
+}: {
+  children: React.ReactNode;
+  trigger: boolean;
+  onClose: any;
+}) {
+  const divRef = useRef(null);
+  const [isOpened, setIsOpened] = useState(false);
+  useEffect(() => {
+    setIsOpened(trigger);
+  }, [trigger]);
+  return (
+    <div
+      onClick={onClose}
+      className={`${
+        isOpened ? "visible opacity-100" : "invisible opacity-0"
+      } transition-all duration-500 w-full h-screen bg-black/40 px-7 z-50 backdrop-blur-sm fixed flex justify-center items-center inset-0`}
+    >
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        ref={divRef}
+        className="max-w-lg w-full px-8 py-4 bg-white rounded-lg"
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
