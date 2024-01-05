b12d0808388446c9062b2dc480cf1cc7a6726607
Didi Prasetyo <didiprasetyo36@gmail.com>
2024-01-05 14:45:54 +0700

    group page

---
+++ app/group/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { FetchAPI, SetCookie, SetGroup } from "@/components/Helper";
+
+export default function Group() {
+  interface GroupDTO {
+    id: number;
+    name: string;
+    code: string;
+    created_at: number;
+    owner: number;
+    active_period: number;
+    members: [];
+  }
+  const { push } = useRouter();
+  const [groups, setGroups] = useState<GroupDTO[]>([]);
+  async function getGroups() {
+    const endpoint = "http://127.0.0.1:8000/chat/groups";
+
+    const list_group = await FetchAPI(endpoint, "GET");
+
+    setGroups(list_group);
+  }
+
+  function redirectToChat(group: Group) {
+    const groupId = group.id;
+    const groupCode = group.code;
+
+    SetCookie("groupId", groupId);
+    SetCookie("topic", "group/" + groupCode);
+    SetGroup(group)
+    push("/chat");
+  }
+
+  const [isHitAAPI, setIsHitAPI] = useState(false);
+
+  useEffect(() => {
+    setIsHitAPI(true);
+  }, []);
+
+  useEffect(() => {
+    if (!isHitAAPI) {
+      getGroups();
+    }
+  }, []);
+
+  return (
+    <main>
+      <h1>Groups</h1>
+      <ul>
+        {groups.map((item, key) => (
+          <div
+            key={key}
+            onClick={() =>
+              redirectToChat({
+                id: item.id,
+                name: item.name,
+                code: item.code,
+                activePeriod: item.active_period,
+              })
+            }
+          >
+            {item.name}
+          </div>
+        ))}
+      </ul>
+    </main>
+  );
+}
